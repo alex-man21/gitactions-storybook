19f91357db2f4b62b8b747361845610f97fe262b
Alex Man <alex.man@applitools.com>
2021-11-05 07:53:48 -0700

    Updated yaml

--- applitools.config.js
+++ applitools.config.js
@@ -3,7 +3,8 @@ module.exports = {
   browser: [
     {width: 1280, height: 1024, name: 'ie11'},
     {width: 1280, height: 1024, name: 'chrome'}
-  ]
+  ],
+  batchId: process.env.APPLITOOLS_BATCH_ID
   // visualGridOptions: {
   //   polyfillAdoptedStyleSheets: true,
   //   ieV2: true    
