5c9e4bb5fbdb40df29ed4782144e18df9c4b9c92
Alex Man <alex.man@applitools.com>
2021-11-05 00:15:24 -0700

    Initial commit

---
+++ applitools.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  concurrency: 2,
+  browser: [
+    {width: 1280, height: 1024, name: 'ie11'},
+    {width: 1280, height: 1024, name: 'chrome'}
+  ]
+  // visualGridOptions: {
+  //   polyfillAdoptedStyleSheets: true,
+  //   ieV2: true    
+  // }
+}
