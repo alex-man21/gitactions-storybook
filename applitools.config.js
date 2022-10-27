28bcce90c76069aa5efb36b23e80431b4a385c1b
Alex Man <alex.man@applitools.com>
2022-10-27 09:40:53 -0700

    Adding browser

--- applitools.config.js
+++ applitools.config.js
@@ -4,7 +4,8 @@ module.exports = {
   concurrency: 2,
   browser: [
     {width: 1280, height: 1024, name: 'ie11'},
-    {width: 1280, height: 1024, name: 'chrome'}
+    {width: 1280, height: 1024, name: 'chrome'},
+    {width: 1280, height: 1024, name: 'firefox'}
   ],
   apiKey: process.env.APPLITOOLS_API_KEY,
   batchId: process.env.APPLITOOLS_BATCH_ID,
