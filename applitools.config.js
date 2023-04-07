1d7d769327bd3a0eed74242e1e7d11d354bda661
Alex Man <alex.man@applitools.com>
2023-04-07 11:04:04 -0700

    testing automated batch closing

--- applitools.config.js
+++ applitools.config.js
@@ -9,7 +9,9 @@ module.exports = {
   ],
   apiKey: process.env.APPLITOOLS_API_KEY,
   batchId: process.env.APPLITOOLS_BATCH_ID,
-  batchName: `${process.env.GITHUB_EVENT} gitactions_storybook: ${process.env.APPLITOOLS_BATCH_ID}`
+  batchName: `${process.env.GITHUB_EVENT} gitactions_storybook: ${process.env.APPLITOOLS_BATCH_ID}`,
+  storybookUrl: `http://localhost.com:9009`,
+  include: "storytitle"
   // visualGridOptions: {
   //   polyfillAdoptedStyleSheets: true,
   //   ieV2: true    
