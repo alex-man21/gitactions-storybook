ba258858755212a0783f0883424f822a041acbc6
alex <alex.man@applitools.com>
2024-02-06 10:55:54 -0800

    testing pr integration

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ class App extends Component {
             rel="noopener noreferrer"
           >
             Learn Reacts in a totally new way!
+            Third times the charm :D
           </a>
         </header>
       </div>
